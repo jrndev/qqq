5e21c91d44107ee0473851405183c24a6dd6fbfd
Jarno Metsaranta <jarno.metsaranta@gmail.com>
2021-02-25 10:35:25 +0200

    heroku stuff part 2

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080;
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('asd')
 })
 
 app.listen(port, () => {
