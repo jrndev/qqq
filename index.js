411493e8593d4795a5fabf5ff98300e4b882150c
Jarno Metsaranta <jarno.metsaranta@gmail.com>
2021-02-25 11:21:37 +0200

    lab04 & extra

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('asd')
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+
+app.get('/api/locations', (req, res) => {
+  const param = req.params.id
+  res.header("Content-Type",'application/json')
+  res.json(database)
+})
+
+app.get('/api/locations/:p', (req, res) => {
+  res.header("Content-Type",'application/json')
+  req.params.p === 'pretty' ? res.send(JSON.stringify(database, null, 4)) : res.json(database)
 })
 
 app.listen(port, () => {
